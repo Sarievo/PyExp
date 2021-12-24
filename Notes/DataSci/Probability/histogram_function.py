d04bbe5c9eba6bb35db66cf13be8a521e1a4ed16
Sarievo <contact@sa-corp.me>
2021-12-24 23:04:54 +0800

    update notes (stats)

--- Notes/DataSci/Probability/histogram_function.py
+++ Notes/DataSci/Probability/histogram_function.py
@@ -3,6 +3,6 @@ import numpy as np
 
 
 def histogram_function(rand_vars):
-    plt.hist(rand_vars, bins = np.arange(len(set(rand_vars)))-0.5, edgecolor = "black")
+    plt.hist(rand_vars, bins=np.arange(len(set(rand_vars))) - 0.5, edgecolor="black")
     plt.xticks(list(range(rand_vars.max())))
     plt.show()
--- Notes/DataSci/Probability/poisson_dristribution_expectation.py
+++ Notes/DataSci/Probability/poisson_dristribution_expectation.py
@@ -2,5 +2,5 @@ import scipy.stats as stats
 
 # generate random variable
 # stats.poisson.rvs(lambda, size = num_values)
-rvs = stats.poisson.rvs(10, size = 1000)
+rvs = stats.poisson.rvs(10, size=1000)
 print(rvs)
